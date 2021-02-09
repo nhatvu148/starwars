396b5d2baad7f9f46be9be5386b244ecd52d18bc
Akiyama <nhatvu148@gmail.com>
2021-02-09 13:30:15 +0900

    keepPreviousData: true

--- src/components/People.jsx
+++ src/components/People.jsx
@@ -1,26 +1,35 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import { useQuery } from "react-query";
 import Axios from "axios";
 import Person from "./Person";
 
-const fetchPeople = async () => {
-  const res = await Axios.get("http://swapi.dev/api/people/");
+const fetchPeople = async (page) => {
+  const res = await Axios.get(`http://swapi.dev/api/people/?page=${page}`);
   return res;
 };
 
 const People = () => {
-  const { data, status } = useQuery("people", fetchPeople);
-  console.log(data);
-  //   useEffect(() => {
-  //     (async () => {
-  //       const res = await Axios.get("http://swapi.dev/api/planets/");
-  //       console.log(res);
-  //     })();
-  //   }, []);
+  const [page, setPage] = useState(2);
+  const { data, status } = useQuery(["people", page], () => fetchPeople(page), {
+    keepPreviousData: true,
+  });
 
   return (
     <div>
       <h2>People</h2>
+      <button
+        onClick={() => setPage((old) => Math.max(old - 1, 1))}
+        disabled={page === 1}
+      >
+        Previous page
+      </button>
+      <span>{page}</span>
+      <button
+        onClick={() => setPage((old) => old + 1)}
+        disabled={data && !data.data.next}
+      >
+        Next page
+      </button>
       {status === "loading" && <div>Loading data ...</div>}
       {status === "error" && <div>Error fetching data</div>}
       {status === "success" && (
--- src/components/Planets.jsx
+++ src/components/Planets.jsx
@@ -3,26 +3,25 @@ import { useQuery } from "react-query";
 import Axios from "axios";
 import Planet from "./Planet";
 
-const fetchPlanets = async (greeting, page) => {
-  console.log(greeting);
+const fetchPlanets = async (page) => {
   const res = await Axios.get(`http://swapi.dev/api/planets/?page=${page}`);
   return res;
 };
 
 const Planets = () => {
   const [page, setPage] = useState(1);
-  const greeting = "hello, Nhat Vu";
   const { data, status } = useQuery(
-    ["planets", greeting, page],
-    () => fetchPlanets(greeting, page),
+    ["planets", page],
+    () => fetchPlanets(page),
     {
       staleTime: 0,
       // cacheTime: 10,
+      keepPreviousData: true,
       onSuccess: () => console.log("data fetched with no problem"),
       onError: () => console.log("error occurred when fetching data"),
     }
   );
-  console.log(data);
+
   //   useEffect(() => {
   //     (async () => {
   //       const res = await Axios.get("http://swapi.dev/api/planets/");
@@ -33,9 +32,19 @@ const Planets = () => {
   return (
     <div>
       <h2>Planets</h2>
-      <button onClick={() => setPage(1)}>Page 1</button>
+      {/* <button onClick={() => setPage(1)}>Page 1</button>
       <button onClick={() => setPage(2)}>Page 2</button>
-      <button onClick={() => setPage(3)}>Page 3</button>
+      <button onClick={() => setPage(3)}>Page 3</button> */}
+      <button
+        onClick={() => setPage((old) => Math.max(old - 1, 1))}
+        disabled={page === 1}
+      >
+        Previous page
+      </button>
+      <span>{page}</span>
+      <button onClick={() => setPage((old) => old + 1)} disabled={data && !data.data.next}>
+        Next page
+      </button>
       {status === "loading" && <div>Loading data ...</div>}
       {status === "error" && <div>Error fetching data</div>}
       {status === "success" && (

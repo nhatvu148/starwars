b440f68a099e311781c19181888abaf71d207662
Akiyama <nhatvu148@gmail.com>
2021-02-09 12:27:45 +0900

    add people and planets

--- src/components/People.jsx
+++ src/components/People.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useQuery } from "react-query";
+import Axios from "axios";
+import Person from "./Person";
+
+const fetchPeople = async () => {
+  const res = await Axios.get("http://swapi.dev/api/people/");
+  return res;
+};
 
 const People = () => {
+  const { data, status } = useQuery("people", fetchPeople);
+  console.log(data);
+  //   useEffect(() => {
+  //     (async () => {
+  //       const res = await Axios.get("http://swapi.dev/api/planets/");
+  //       console.log(res);
+  //     })();
+  //   }, []);
+
   return (
     <div>
       <h2>People</h2>
+      {status === "loading" && <div>Loading data ...</div>}
+      {status === "error" && <div>Error fetching data</div>}
+      {status === "success" && (
+        <div>
+          {data.data.results.map((person) => (
+            <Person key={person.name} person={person} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- src/components/Planets.jsx
+++ src/components/Planets.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useQuery } from "react-query";
+import Axios from "axios";
+import Planet from "./Planet";
+
+const fetchPlanets = async () => {
+  const res = await Axios.get("http://swapi.dev/api/planets/");
+  return res;
+};
 
 const Planets = () => {
+  const { data, status } = useQuery("planets", fetchPlanets);
+  console.log(data);
+  //   useEffect(() => {
+  //     (async () => {
+  //       const res = await Axios.get("http://swapi.dev/api/planets/");
+  //       console.log(res);
+  //     })();
+  //   }, []);
+
   return (
     <div>
       <h2>Planets</h2>
+      {status === "loading" && <div>Loading data ...</div>}
+      {status === "error" && <div>Error fetching data</div>}
+      {status === "success" && (
+        <div>
+          {data.data.results.map((planet) => (
+            <Planet key={planet.name} planet={planet} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };

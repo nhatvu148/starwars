163aad218ec6a2de99daf27b22cf3e3fa9042369
Akiyama <nhatvu148@gmail.com>
2021-02-09 13:00:14 +0900

    query variables

--- src/components/Planets.jsx
+++ src/components/Planets.jsx
@@ -1,19 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import Axios from "axios";
 import Planet from "./Planet";
 
-const fetchPlanets = async () => {
-  const res = await Axios.get("http://swapi.dev/api/planets/");
+const fetchPlanets = async (greeting, page) => {
+  console.log(greeting);
+  const res = await Axios.get(`http://swapi.dev/api/planets/?page=${page}`);
   return res;
 };
 
 const Planets = () => {
-  const { data, status } = useQuery("planets", fetchPlanets, {
-    staleTime: 0,
-    // cacheTime: 10,
-    onSuccess: () => console.log("data fetched with no problem"),
-  });
+  const [page, setPage] = useState(1);
+  const greeting = "hello, Nhat Vu";
+  const { data, status } = useQuery(
+    ["planets", greeting, page],
+    () => fetchPlanets(greeting, page),
+    {
+      staleTime: 0,
+      // cacheTime: 10,
+      onSuccess: () => console.log("data fetched with no problem"),
+      onError: () => console.log("error occurred when fetching data"),
+    }
+  );
   console.log(data);
   //   useEffect(() => {
   //     (async () => {
@@ -25,6 +33,9 @@ const Planets = () => {
   return (
     <div>
       <h2>Planets</h2>
+      <button onClick={() => setPage(1)}>Page 1</button>
+      <button onClick={() => setPage(2)}>Page 2</button>
+      <button onClick={() => setPage(3)}>Page 3</button>
       {status === "loading" && <div>Loading data ...</div>}
       {status === "error" && <div>Error fetching data</div>}
       {status === "success" && (
